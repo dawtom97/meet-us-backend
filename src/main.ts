0e4f6017a8c90dab48805d338898cbd48396adc2
Dawid <dawtom97@gmail.com>
2022-10-12 06:40:18 +0200

    posts controller

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,6 @@ import * as session from 'express-session'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin:'http://localhost:3000',
-    credentials:true
-  })
-
-  app.use(cookieParser());
-  // app.use(
-  //   session({
-  //     secret: '7348d8683',
-  //     resave: false,
-  //     saveUninitialized: false,
-  //   }),
-  // );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
 }
